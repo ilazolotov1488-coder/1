61eb2f9b43d6ef0fb00e377c34e9f9b9bfa6c373
SpaceVisuals Dev <dev@spacevisuals.local>
2026-04-24 15:33:54 +0300

    logotype 11x7 fit in watermark

--- src/main/java/space/visuals/client/hud/elements/component/WatermarkComponent.java
+++ src/main/java/space/visuals/client/hud/elements/component/WatermarkComponent.java
@@ -43,12 +43,12 @@ public class WatermarkComponent extends DraggableHudElement {
         elements.add(new HudElement("s", ()->"","") {
             @Override
             public void calculateWidth(Font font, float iconSize, float cellPadding, float iconTextSpacing) {
-                this.width = cellPadding * 2 + 14f;
+                this.width = cellPadding * 2 + 11f;
             }
 
             @Override
             public void drawContent(CustomDrawContext ctx, float blockX, float blockY, float blockHeight, float iconSize, float iconTextSpacing, ColorRGBA iconColor, ColorRGBA textColor, Font font) {
-                float logoW = 14f, logoH = 10f;
+                float logoW = 11f, logoH = 7f;
                 float logoX = blockX + (getWidth() - logoW) / 2f;
                 float logoY = blockY + (blockHeight - logoH) / 2f;
                 DrawUtil.drawRoundedTexture(ctx.getMatrices(), Zenith.id("icons/logotype.png"),
